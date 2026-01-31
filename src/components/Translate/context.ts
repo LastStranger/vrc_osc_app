622870f5b04739e303e4aa3a60ef6aa2a572c45a
laststranger <callmelz@qq.com>
2026-01-31 14:55:17 +0800

    删除无效文件

--- src/components/Translate/context.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { TranslateContextT } from "@/components/Translate/types";
-
-export const TranslateContext = createContext<TranslateContextT>({} as TranslateContextT);
--- src/components/Translate/types.ts
+++
@@ -1,5 +0,0 @@
-import TranslateStore from "@/store/translateStore";
-
-export type TranslateContextT ={
-    store: TranslateStore;
-}
